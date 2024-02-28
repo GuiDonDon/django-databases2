362735c2393f91c9b3d4c9d56beaad40eec5fb92
GuiDonDon <shatunbadun@gmail.com>
2024-02-28 21:48:02 +0300

    first commit

---
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            try:
+                count += form.cleaned_data['is_main']
+            except KeyError:
+                pass
+        if count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif count < 1:
+            raise ValidationError('Нужно указать основной раздел')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+
